025ba9efdfa4d74d9be4159dd1ebb0de7c27e40b
expandedworld <gkscksgml8156@gmail.com>
2021-08-18 03:50:21 +0900

    pictures 페이지 구성 및 자바스크립트로 클릭 이벤트 추가

---
+++ js/detail.js
@@ -0,0 +1,129 @@
+var view = document.querySelectorAll("#view");
+var detail = document.querySelectorAll("#detail");
+
+
+for(let i=0; i < view.length; i++) {
+        view[i].addEventListener("click", function(){
+        let detail_item = detail[i]
+        detail_item.style.display = "block";
+        view[i].style.display = "none";
+        });
+}
+
+function cl() {
+    for(i=0; i < detail.length; i++){
+        let detail_item2 = detail[i]
+        detail_item2.style.display = "none";
+        view[i].style.display = "block";
+    }
+}
+
+
+
+
+
+
+
+
+
+/*else {
+    view[i].addEventListener("click", function(){
+    let detail_item2 = detail[i]
+    detail_item2.style.display = "none";
+    view[i].innerText = "상세 설명 보기";
+    isOpen = false;
+    });
+}*/
+
+
+/*var view2 = document.querySelector("#view2");
+let isOpen2 = false;
+view2.addEventListener("click", function(){
+    if(isOpen2 == false) {
+        document.querySelector("#detail2").style.display = "block";
+        view2.innerText = "상세 설명 닫기";
+        isOpen2 = true;
+    }
+    else {
+        document.querySelector("#detail2").style.display = "none";
+        view2.innerText = "상세 설명 보기";
+        isOpen2 = false;
+    }
+});
+
+
+var view3 = document.querySelector("#view3");
+let isOpen3 = false;
+view3.addEventListener("click", function(){
+    if(isOpen3 == false) {
+        document.querySelector("#detail3").style.display = "block";
+        view3.innerText = "상세 설명 닫기";
+        isOpen3 = true;
+    }
+    else {
+        document.querySelector("#detail3").style.display = "none";
+        view3.innerText = "상세 설명 보기";
+        isOpen3 = false;
+    }
+});
+
+
+var view4 = document.querySelector("#view4");
+let isOpen4 = false;
+view4.addEventListener("click", function(){
+    if(isOpen4 == false) {
+        document.querySelector("#detail4").style.display = "block";
+        view4.innerText = "상세 설명 닫기";
+        isOpen4 = true;
+    }
+    else {
+        document.querySelector("#detail4").style.display = "none";
+        view4.innerText = "상세 설명 보기";
+        isOpen4 = false;
+    }
+});
+
+var view5 = document.querySelector("#view5");
+let isOpen5 = false;
+view5.addEventListener("click", function(){
+    if(isOpen5 == false) {
+        document.querySelector("#detail5").style.display = "block";
+        view5.innerText = "상세 설명 닫기";
+        isOpen5 = true;
+    }
+    else {
+        document.querySelector("#detail5").style.display = "none";
+        view5.innerText = "상세 설명 보기";
+        isOpen5 = false;
+    }
+});
+
+var view6 = document.querySelector("#view6");
+let isOpen6 = false;
+view6.addEventListener("click", function(){
+    if(isOpen6 == false) {
+        document.querySelector("#detail6").style.display = "block";
+        view6.innerText = "상세 설명 닫기";
+        isOpen6 = true;
+    }
+    else {
+        document.querySelector("#detail6").style.display = "none";
+        view6.innerText = "상세 설명 보기";
+        isOpen6 = false;
+    }
+});
+
+var view7 = document.querySelector("#view7");
+let isOpen7 = false;
+view7.addEventListener("click", function(){
+    if(isOpen7 == false) {
+        document.querySelector("#detail7").style.display = "block";
+        view7.innerText = "상세 설명 닫기";
+        isOpen7 = true;
+    }
+    else {
+        document.querySelector("#detail7").style.display = "none";
+        view7.innerText = "상세 설명 보기";
+        isOpen7 = false;
+    }
+});*/

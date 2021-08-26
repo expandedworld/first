f4d50e2ead7e22f3be37869b7279278ac2e2ae07
expandedworld <gkscksgml8156@gmail.com>
2021-08-26 18:56:52 +0900

    book페이지 추가 및 배경 색, 메뉴, 아이콘 등 수정

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,19 @@
 /*toggle로 css 바꿔치기*/
 
 var element = document.getElementById("menu");
+var open_menu = document.getElementById("open_menu");
+var close_menu = document.getElementById("close_menu");
 
 function toggle() {
 	element.classList.toggle("menu-click");
 }
+
+open_menu.addEventListener("click",function(){
+	open_menu.style.display = "none";
+	close_menu.style.display = "block";
+});
+
+close_menu.addEventListener("click",function(){
+	open_menu.style.display = "block";
+	close_menu.style.display = "none";
+});
